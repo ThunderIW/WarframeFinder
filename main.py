2bfbcb000d9ea9132ca3e376d662454f6ae564ec
ThunderIW <im.wiessler@gmail.com>
2022-04-27 16:17:26 -0700

    test

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
+import json
+
 import requests
-planets={"Mercury":
-             ["Caloris","Lares","Odin","Apollodorus",
-                    "Elion","Suisei","Tolstoj"],
-         "Venus":["Montes","V Prime","Cytherean"],
-         }
-planetsList=["Mercury","Venus","Earth","Mars","Phobos","Deimos","Ceres","Jupiter",
-             "Europa","Saturn","Uranus"]
 
+warframeItems = []
+with open('WarframeItem.json') as json_file:
+    data = json.load(json_file)
 
-response=requests.get("http://drops.warframestat.us/data/all.json")
+    for items in range(0, len(data)):
+        warframeItems.append(data[items]["item_name"])
 
-relic_list=response.json()
+planets = {"Mercury":
+               ["Caloris", "Lares", "Odin", "Apollodorus","Elion", "Suisei", "Tolstoj"],
+           "Venus": ["Montes", "V Prime", "Cytherean","Linea","Ishtar (Caches)"]
+           }
+planetsList = ["Mercury", "Venus", "Earth", "Mars", "Phobos", "Deimos", "Ceres", "Jupiter",
+               "Europa", "Saturn", "Uranus"]
+
+response = requests.get("http://drops.warframestat.us/data/all.json")
+missonRewards=requests.get("http://drops.warframestat.us/data/missionRewards.json").json()
+
+relic_list = response.json()
+##print(missonRewards["missionRewards"]["Mercury"])
+
 
 ##print(planent_selected)
 ##print(relic_list["missionRewards"]["Mercury"][planent_selected[0]])
 
 
-
 def getFarmlocation(relic: str):
-
-    print(f"ItemName:{relic}")
-
     for planet in planetsList:
         try:
-            chosen_planet=planets.get(planet)
-            ##print(planet)
+            chosen_planet = planets.get(planet)
+
             ##print(chosen_planet)
             for planetNodes in chosen_planet:
-                gamemode=relic_list["missionRewards"][planet][planetNodes]['gameMode']
-                rewards=relic_list["missionRewards"][planet][planetNodes]["rewards"]
-
-                if len(rewards["A"])!=0:
-                    for infoA in rewards["A"]:
-                        if infoA["itemName"]==relic:
-                            chance = infoA["chance"]
-                            print(f"Rotation:A\nPlanet:{planet}\nPlanetNode:{planetNodes}\nGameMode:{gamemode}\nChance:{chance}%")
-
-
-                if len(rewards["B"])!=0:
-                    for infoB in rewards["B"]:
-                        if infoB["itemName"] == relic:
-                            chance=infoB["chance"]
-                            print(f"Rotation:B\nPlanet:{planet}\nPlanetNode:{planetNodes}\nGameMode:{gamemode}\nChance:{chance}%")
-                            ##print(planet, planetNodes,gamemode)
-
-                print("---------------------------------------------------------------------------------------")
+                gamemode =missonRewards["missionRewards"][planet][planetNodes]['gameMode']
+                rewards = missonRewards["missionRewards"][planet][planetNodes]["rewards"]
+                print(planetNodes,gamemode,rewards["A"])
 
 
 
@@ -60,45 +52,20 @@ def getFarmlocation(relic: str):
 
 
 
-
-
-
 def findPrimePartLocation(part: str):
     relics = relic_list["relics"]
-    for relic in range(0,len(relics)):
+    for relic in range(0, len(relics)):
         for rewards in relics[relic]["rewards"]:
-            if rewards["itemName"]==part:
-                tier=relics[relic]["tier"]
-                relicName=relics[relic]["relicName"]
-                state=relics[relic]["state"]
-                rarity=rewards["rarity"]
-                chanceOfGettingItem=rewards["chance"]
+            if rewards["itemName"] == part:
+                tier = relics[relic]["tier"]
+                relicName = relics[relic]["relicName"]
+                state = relics[relic]["state"]
+                rarity = rewards["rarity"]
+                chanceOfGettingItem = rewards["chance"]
                 print(part)
-                print(f"Name:{tier+' '+relicName}\nState:{state}\nRarity:{rarity}\nChance:{chanceOfGettingItem}%")
+                print(f"Name:{tier + ' ' + relicName}\nState:{state}\nRarity:{rarity}\nChance:{chanceOfGettingItem}%")
                 print("----------------------------------------------------------")
 
 
 
-
-
-
-
-
-
-
-
-
-
-getFarmlocation("FFFF")
-
-
-
-
-
-
-
-
-
-
-
-
+getFarmlocation("Magazine Warp")
